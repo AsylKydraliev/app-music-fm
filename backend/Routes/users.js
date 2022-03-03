c9e9f3dd1f5a66587207a5c9d7ea4ff6a1cc43ba
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-03-03 16:11:51 +0600

    Backend. User post sessions.

--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('../models/User');
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
@@ -23,6 +24,30 @@ router.post('/', async (req, res, next)=>{
        }
        return next(error);
    }
+});
+
+router.post('/sessions', async (req, res, next) => {
+    try{
+        const user = await User.findOne({username: req.body.username});
+
+        if(!user){
+            return res.status(400).send({error: 'Wrong data'});
+        }
+
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+        if(!isMatch) {
+            return res.status(400).send({error: 'Wrong data'});
+        }
+
+        return res.send({message: 'Data is correct'});
+    }catch (error){
+        if(error instanceof mongoose.Error.ValidationError){
+            return res.status(400).send(error);
+        }
+        return next(error);
+   }
+
 });
 
 module.exports = router;
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
     username: {
@@ -10,8 +11,30 @@ const UserSchema = new Schema({
     password: {
         type: String,
         required: true,
+    },
+    token: {
+        type: String,
+        required: true
     }
 });
+
+const SALT_WORK_FACTOR = 10;
+
+UserSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.set('toJSON', {
+    transform: (doc, ret, options) => {
+        delete ret.password;
+        return ret;
+    }
+})
+
 const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
