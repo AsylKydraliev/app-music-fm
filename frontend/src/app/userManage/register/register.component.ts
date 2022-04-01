65d5f342b08171cb6dd4092d483c5c3bc169e020
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-04-01 18:44:08 +0600

    Register with google.

--- frontend/src/app/userManage/register/register.component.ts
+++ frontend/src/app/userManage/register/register.component.ts
@@ -2,10 +2,10 @@ import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular
 import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/types';
-import { fbLoginUserData, RegisterError } from '../../models/user.model';
+import { fbLoginUserData, googleLoginUserData, RegisterError } from '../../models/user.model';
 import { Observable, Subscription } from 'rxjs';
-import { loginFbRequest, registerUserRequest } from '../../store/users/users.actions';
-import { FacebookLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { loginFbRequest, loginGoogleRequest, registerUserRequest } from '../../store/users/users.actions';
+import { FacebookLoginProvider, GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
 
 @Component({
   selector: 'app-register',
@@ -19,7 +19,10 @@ export class RegisterComponent implements AfterViewInit, OnInit, OnDestroy {
   loading: Observable<boolean>;
   fbLoading: Observable<boolean>;
   authStateSub!: Subscription;
-  userData!: fbLoginUserData;
+  fbUserData!: fbLoginUserData;
+  googleUserData!: googleLoginUserData;
+  fbLoginClick = false;
+  googleLoginClick = false;
 
   constructor(private store: Store<AppState>, private auth: SocialAuthService) {
     this.error = store.select(state => state.users.registerError);
@@ -42,15 +45,32 @@ export class RegisterComponent implements AfterViewInit, OnInit, OnDestroy {
     this.authStateSub = this.auth.authState.subscribe((user: SocialUser) => {
       if(!user) return;
 
-      this.userData = {
-        authToken: user.authToken,
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        avatar: user.response.picture.data.url,
+      if(this.googleLoginClick){
+        if(user.provider === "GOOGLE"){
+          this.googleUserData = {
+            authToken: user.authToken,
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            avatar: user.photoUrl,
+            accessToken: user.response.access_token
+          }
+        }
+        this.store.dispatch(loginGoogleRequest({userData: this.googleUserData}));
       }
 
-      this.store.dispatch(loginFbRequest({userData: this.userData}));
+      if(this.fbLoginClick){
+        if(user.provider === "FACEBOOK"){
+          this.fbUserData = {
+            authToken: user.authToken,
+            id: user.id,
+            email: user.email,
+            name: user.name,
+            avatar: user.response.picture.data.url
+          }
+        }
+        this.store.dispatch(loginFbRequest({userData: this.fbUserData}));
+      }
     })
   }
 
@@ -59,7 +79,13 @@ export class RegisterComponent implements AfterViewInit, OnInit, OnDestroy {
   }
 
   fbLogin(){
+    this.fbLoginClick= true;
     void this.auth.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  googleLogin(){
+    this.googleLoginClick = true;
+    void this.auth.signIn(GoogleLoginProvider.PROVIDER_ID);
   }
 
   ngOnDestroy() {
